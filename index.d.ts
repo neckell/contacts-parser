b67e6398409a0186a7d0b1a29eb8d9bec61b151a
Nicolas Eckell <niceckell@gmail.com>
2023-09-20 01:46:08 -0300

    modify ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 export function parseContacts(file: any, countryCode: string): Promise<Uint8Array>;
-export function parseEmail(email: string, firstName: string, lastName: string, inboxDesired: string): Promise<string>;
-export function parsePhone(phone: string, countryCode: string): Promise<string>;
+export function parseEmail(email: string, firstName: string, lastName: string, inboxDesired: string): string;
+export function parsePhone(phone: string, countryCode: string): string;
